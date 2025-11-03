b34f834d9a6c3d60f48fc474afc9a9815a8abc94
alamtalukder <shahalamcse7@gmail.com>
2025-11-03 08:59:08 +0600

    done

---
+++ intermidateJS.js/arraysplit.js
@@ -0,0 +1,11 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+// const part = nums.slice( 2, 4); 
+// //console.log(part); 
+
+// const number = [ 23, 24, 25, 26, 27, 28, 29]; 
+// const part2 = number.slice(1, 4); 
+// console.log(part2); 
+
+const studentId = [ 2, 3, 4, 5, 6, 7, 9]; 
+const result = studentId.join(" "); 
+console.log(result); 
---
+++ intermidateJS.js/break-continue.js
@@ -0,0 +1,7 @@
+const number =[2, 3, 4, 5, 6, 7, 8]; 
+for(let i = 0; i<number.length; i++){
+    console.log(number[i]); 
+    if(number[i] > 0){
+        break; 
+    }
+}
