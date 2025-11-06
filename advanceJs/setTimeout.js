dcb8623e47754035eb68bcf5a3dd29da4e0e0118
alamtalukder <shahalamcse7@gmail.com>
2025-11-06 09:23:09 +0600

    done

---
+++ advanceJs/setTimeout.js
@@ -0,0 +1,18 @@
+function doSomething(){
+    console.log(4444); 
+}
+
+// console.log(2222); 
+// console.log(1111); 
+// setTimeout(doSomething);
+
+// setTimeout( function (){
+//     console.log("You are late"); 
+// } , 2000)  
+
+// setTimeout( () => console.log("You are late"), 4000); 
+
+// console.log(2222); 
+// console.log(1111);
+
+setInterval( () => console.log("Hello every 3 seconds"), 3000);
